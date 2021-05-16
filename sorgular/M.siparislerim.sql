c8c0d5cc540dbef99679236c738df042aa8c5d74
Kuvarti <uzali.kafasi@gmail.com>
2021-05-16 18:01:01 +0300

    siparislerim sorgulari

---
+++ sorgular/M.siparislerim.sql
@@ -0,0 +1,6 @@
+/*Magaza için Siparislerim */ 
+
+select s.siprarisid,u.uisim,u.uid,k.isim,k.eposta,s.tarih,s.durum
+from siparis as s, magaza as m, satis as sat, kullanici as k,urun as u
+where (s.sid=sat.satisid and m.magazaid=sat.mid and s.kid=k.id and sat.uid=u.uid) and m.magazaid=1
+order by durum
