0b3f4efd06e460ea07a306147c3911ad45210d51
Kuvarti <uzali.kafasi@gmail.com>
2021-05-20 18:17:13 +0300

    menü sorgusu

---
+++ New folder/sorgular/menü listeleme.sql
@@ -0,0 +1,3 @@
+select u.uisim, u.resim, sat.fiyat, m.magazaisim
+from satis as sat, urun as u, magaza as m, kategori as k
+where (sat.uid=u.uid and sat.mid=m.magazaid) and u.kategori_id=(select k_id from kategori where k_ad='<kategoriad>')
