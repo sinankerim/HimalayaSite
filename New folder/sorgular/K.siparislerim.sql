34329a9f27255d45f7de0b0025bc23803776ff35
Kuvarti <uzali.kafasi@gmail.com>
2021-05-21 23:57:18 +0300

    birtakım degisiklikler

--- New folder/sorgular/K.siparislerim.sql
+++ New folder/sorgular/K.siparislerim.sql
@@ -1,3 +1,5 @@
 /*Kullanici için Siparislerim */ 
-
-select * from siparis where kid=3
+use eticsitedb
+select s.siprarisid, s.sid,u.uisim, s.tarih, d.durum, s.adet
+from siparis as s, durum as d, satis as sat, urun as u
+where (d.id=s.durum and s.sid=sat.satisid and sat.uid=u.uid) and kid=3
--- New folder/sorgular/adminpaneli/magaza - sadece siparis verildi durumundaki siparisler.sql
+++ New folder/sorgular/adminpaneli/magaza - sadece siparis verildi durumundaki siparisler.sql
@@ -3,5 +3,5 @@ USE eticsitedb
 
 select s.siprarisid, s.adet, u.uisim,u.uid,k.isim,k.eposta,s.tarih,s.durum
 from siparis as s, magaza as m, satis as sat, kullanici as k,urun as u
-where (s.sid=sat.satisid and m.magazaid=sat.mid and s.kid=k.id and sat.uid=u.uid) and (m.magazaid=1 and s.durum='hazirlaniyor')
+where (s.sid=sat.satisid and m.magazaid=sat.mid and s.kid=k.id and sat.uid=u.uid) and (m.magazaid=1 and s.durum=2)
 order by durum
