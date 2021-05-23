9900426eb2e8bc14fadeeedd323344b6bdaba85a
Kuvarti <uzali.kafasi@gmail.com>
2021-05-23 12:02:48 +0300

    mssql mysql dönüşümü

--- New folder/sorgular/adminpaneli/magaza - genel bilgiler güncelleme(hepsi).sql
+++ New folder/sorgular/adminpaneli/magaza - genel bilgiler güncelleme(hepsi).sql
@@ -5,10 +5,7 @@ update magaza set adres='0' where magazaid=0 /*magaza adres değiştirme*/;
 update magaza set resim='0' where magazaid=0 /*magaza logo/resim değiştirme*/;
 
 select * from magaza where magazasifre='0'; /*magaza sifresi geğiştirebilmerk için eski şifreyi sorgulamak için kullanılacak sorgu*/;
-
-if NOT EXISTS(select * from magaza where magazaid=0)  /*magazayi silme magaza ile beraber bütün magaza yorumlarını ve satislari siler*/
-begin
-	delete from magaza where magazaid=0;
-	delete from satis where mid=0;
-	delete from myorum where mid=0;
-end
+ 
+delete from magaza where magazaid=0; 	/*magazayi silme magaza ile beraber bütün magaza yorumlarını ve satislari siler*/
+delete from satis where mid=0;			/*bunuda üste ekle*/
+delete from myorum where mid=0;			/*bunuda üste ekle*/
--- New folder/sorgular/kullanicikayitekleme.sql
+++ New folder/sorgular/kullanicikayitekleme.sql
@@ -1,10 +1,11 @@
+insert into kullanici (eposta, sifre, isim, yas) 
+select '0', '0', '0', 0 from dual
+where NOT EXISTS(select * from kullanici where eposta='0')
+
+/*
 if NOT EXISTS(select * from kullanici where eposta='0')
 begin
 	insert into kullanici (eposta, sifre, isim, yas) 
 	values('0', '0', '0', 0)
 end
-
-
-insert into kullanici (eposta, sifre, isim, yas) 
-select '0', '0', '0', 0 from dual
-where NOT EXISTS(select * from kullanici where eposta='0')
+*/
