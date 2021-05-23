40306746240db9d7fdf7ebabcb15084d8ec50395
Kuvarti <uzali.kafasi@gmail.com>
2021-05-23 03:14:11 +0300

    sepet düzenleme

--- New folder/sorgular/K.siparislerim.sql
+++ New folder/sorgular/K.siparislerim.sql
@@ -1,5 +1,5 @@
 /*Kullanici için Siparislerim */ 
 use eticsitedb
-select s.siprarisid, s.sid,u.uisim, s.tarih, d.durum, s.adet
+select s.siprarisid, s.sid,u.uisim, s.tarih, d.durum, s.adet, sat.fiyat
 from siparis as s, durum as d, satis as sat, urun as u
 where (d.id=s.durum and s.sid=sat.satisid and sat.uid=u.uid) and kid=0
