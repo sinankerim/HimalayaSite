59c25cb13d379951377bfc52078e39b74e59c55d
Kuvarti <uzali.kafasi@gmail.com>
2021-05-23 18:58:52 +0300

    güncelleme

--- New folder/sorgular/urunayrinti - urun ayrinti sayfasi.sql
+++ New folder/sorgular/urunayrinti - urun ayrinti sayfasi.sql
@@ -1,4 +1,5 @@
 /* ürünün kendi özel sayfası */
-select u.resim, u.uisim, u.umarka, s.indirim, s.fiyat, m.magazaisim
+select top 1 u.resim, u.uisim, u.umarka, s.indirim, s.fiyat, m.magazaisim
 from satis as s, magaza as m, urun as u
-where s.mid=m.magazaid and s.uid=u.uid
+where s.mid=m.magazaid and s.uid=u.uid and u.uid=0
+order by s.fiyat 
